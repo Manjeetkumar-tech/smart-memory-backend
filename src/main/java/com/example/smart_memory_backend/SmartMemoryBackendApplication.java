f61d60f8a37bde8900b7bd5c1999926cce589134
Manjeetkumar-tech <manjeetkumarjnv968@gmail.com>
2025-07-22 23:45:27 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/smart_memory_backend/SmartMemoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_memory_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMemoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMemoryBackendApplication.class, args);
+	}
+
+}
