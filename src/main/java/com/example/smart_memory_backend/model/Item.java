7bbdb4e75132aef5b40790cd037eb49b290dbc0f
Manjeetkumar-tech <manjeetkumarjnv968@gmail.com>
2025-11-26 00:56:17 +0900

    added latitude and longitude for location

--- src/main/java/com/example/smart_memory_backend/model/Item.java
+++ src/main/java/com/example/smart_memory_backend/model/Item.java
@@ -12,6 +12,8 @@ public class Item {
     private String title;
     private String description;
     private String location;
+    private Double latitude;
+    private Double longitude;
     private LocalDate date;
     
     @Enumerated(EnumType.STRING)
@@ -46,6 +48,12 @@ public class Item {
 
     public String getLocation() { return location; }
     public void setLocation(String location) { this.location = location; }
+
+    public Double getLatitude() { return latitude; }
+    public void setLatitude(Double latitude) { this.latitude = latitude; }
+
+    public Double getLongitude() { return longitude; }
+    public void setLongitude(Double longitude) { this.longitude = longitude; }
 
     public LocalDate getDate() { return date; }
     public void setDate(LocalDate date) { this.date = date; }
