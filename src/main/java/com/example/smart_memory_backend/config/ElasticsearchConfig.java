6f3bb26db11a2aeafa77236fd6486706ad2b82e4
Manjeetkumar-tech <manjeetkumarjnv968@gmail.com>
2025-12-13 01:46:30 +0900

    changed the elasticsearch version

---
+++ src/main/java/com/example/smart_memory_backend/config/ElasticsearchConfig.java
@@ -0,0 +1,44 @@
+package com.example.smart_memory_backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String uris;
+
+    @Value("${spring.elasticsearch.username:}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password:}")
+    private String password;
+
+    @Override
+    @NonNull
+    public ClientConfiguration clientConfiguration() {
+        ClientConfiguration.MaybeSecureClientConfigurationBuilder builder = ClientConfiguration.builder()
+                .connectedTo(uris.replace("https://", "").replace("http://", ""));
+
+        // Add Basic Auth if credentials are provided
+        if (username != null && !username.isEmpty() && password != null && !password.isEmpty()) {
+            builder.withBasicAuth(username, password);
+        }
+
+        // Use SSL if connecting to HTTPS
+        if (uris.startsWith("https")) {
+            builder.usingSsl();
+        }
+
+        // CRITICAL: Disable compatibility headers for Bonsai (ES 7)
+        return builder.withHeaders(() -> {
+            org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
+            headers.add("Content-Type", "application/json");
+            return headers;
+        }).build();
+    }
+}
